9ec22c2f39e99093d180ae15a84eaa0f30d0a5f2
tjhootman <tjhootman@gmail.com>
2025-06-01 10:33:14 -0400

    Add user input for book url, refactor to export txt files to output folder, update .gitignore

--- audiobooks.py
+++ audiobooks.py
@@ -1,25 +1,47 @@
+import os
 import requests
-from audiobook_processing import get_book_title, clean_text
+from processing import get_book_title, clean_text
 
-# request the raw text of The Great Gatsby
-r = requests.get(r'https://www.gutenberg.org/cache/epub/84/pg84.txt')
-book_content = r.text
+# User Input for the URL
+while True:
+    user_url = input("Please enter the Project Gutenberg URL for the raw text of the book (e.g., https://www.gutenberg.org/cache/epub/76/pg76.txt): ")
+    if user_url.strip(): # Check if the input is not empty
+        break
+    else:
+        print("URL cannot be empty. Please try again.")
 
-# Define your input file path
-# input_book_path = 'my_book.txt'
+print(f"Attempting to download from: {user_url}")
+
+# Request the raw text of the book
+try:
+    r = requests.get(user_url)
+    r.raise_for_status() # Raise an exception for HTTP errors (4xx or 5xx)
+    book_content = r.text
+except requests.exceptions.RequestException as e:
+    print(f"Error fetching URL: {e}")
+    print("Please ensure the URL is correct and accessible.")
+    exit() # Exit the script if there's an error downloading the content
+
+# Define the output directory
+output_directory = './output'
+
+# Create the output directory if it doesn't exist
+os.makedirs(output_directory, exist_ok=True)
 
 # Locate the book title
 book_title = get_book_title(book_content)
 print(f"Detected Book Title: '{book_title}'")
 
 # Write the raw content to a file
-with open(f'{book_title}_raw.txt', 'w', encoding='utf-8') as f:
+with open(f'{output_directory}/{book_title}_raw.txt', 'w', encoding='utf-8') as f:
     f.write(book_content)
+    output_raw_path = f"{output_directory}/{book_title}_raw"
+    print(f"Successfully exported raw text to '{output_raw_path}'")
 
 # Define input and output file paths based on the title
-input_file_path = f"{book_title}_raw.txt"
-output_cleaned_path = f"{book_title}_cleaned.txt"
-output_audio_file_gtts = f"{book_title}_audio_gtts.mp3"
+input_file_path = f"{output_raw_path}.txt"
+output_cleaned_path = f"{output_directory}/{book_title}_cleaned.txt"
+output_audio_file_gtts = f"{output_directory}/{book_title}_audio_gtts.mp3"
 
 # Process the text
 cleaned_book_content = clean_text(input_file_path)
